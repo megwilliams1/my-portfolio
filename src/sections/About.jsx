ec9c5bac9347bdb82596bc95c2708f77e9ff93b2
megwilliams1 <159210249+megwilliams1@users.noreply.github.com>
2026-02-12 10:35:10 -0600

    changed some text colors for light and dark mode

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -41,7 +41,7 @@ export const About = () => {
 
             <h2 className="text-4xl md:text-5xl font-bold leading-tight animate-fade-in animation-delay-100 text-secondary-foreground">
               Building the future,
-              <span className="font-serif italic font-normal text-white">
+              <span className="font-serif italic font-normal dark:text-white light:text-black">
                 {" "}
                 one component at a time.
               </span>
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -63,7 +63,7 @@ export const Hero = () => {
                 <br />
                 experiences with
                 <br />
-                <span className="font-serif italic font-normal text-white">
+                <span className="font-serif italic font-normal dark:text-white light:text-black">
                   precision.
                 </span>
               </h1>
